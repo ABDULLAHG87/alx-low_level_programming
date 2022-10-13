d09418d4c2034936da2e721bb9d43393dfd18607
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-13 09:25:52 -0700

    Task 4 -Corrected code with betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
-/*
- * Task 4 using the puts function
- */
 #include<stdio.h>
 
-
+/**
+ * main - Entry point
+ * Description: Task4- put command
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return (0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
 }
