49e3b04667a9acade73150f85a55d5b4898459cf
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-14 18:46:14 -0700

    Task12

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * Task 12 - Software is eating the World
+ * Return: Always 0 (Sucess)
+ */
+
+int main(void)
+{
+	int digit_1;
+	int digit_2;
+
+	for (digit_1 = 0; digit_1 <= 99; digit_1++)
+	{
+		for (digit_2 = digit_1 + 1; digit_2 <= 99; digit_2++)
+		{
+			putchar('0' + digit_1 / 10);
+			putchar('0' + digit_1 % 10);
+			putchar(32);
+			putchar('0' + digit_2 / 10);
+			putchar('0' + digit_2 % 10);
+			if (digit_1 != 98 || digit_2 != 99)
+				putchar(',');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
