6fd9a548a77073ddfefc19929b4f7cecf115631e
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-18 15:04:51 -0700

    Task 7: last digit Number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of a number
+ * @n: Integer value to be tested
+ * Return: 0 (Success)
+ */
+
+int print_last_digit(int n)
+{
+	int result;
+	int absolute;
+
+	if (n < 0)
+		absolute = n * -1;
+	else
+		absolute = n * 1;
+	result = absolute % 10;
+	return (result);
+}
