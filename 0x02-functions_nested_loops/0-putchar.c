0a69d56f971247fa567e8ab16d22b5061f093eeb
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-18 09:05:39 -0700

    Task 0: _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (Success)
+ */
+void _putchar(void);
+
+int main(void)
+{
+	_putchar();
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,6 @@
+#include<unistd.h>
+
+void _putchar(void)
+{
+	write(1,"_putchar\n",9);
+}
