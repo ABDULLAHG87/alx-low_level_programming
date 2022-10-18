76a64260423cd3c63e11aafa6550b52d7be15e3e
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-18 16:47:43 -0700

    Task 9: Learn your times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ * Return: 0 (Success)
+ */
+
+void times_table(void)
+{
+	int row;
+	int col;
+	int result;
+
+	for (row = 0; row < 10; row++)
+	{
+		for (col = 0; col < 10; col++)
+		{
+			result = row * col;
+			if (result <= 9)
+			{
+				_putchar(' ');
+				_putchar('0' + result);
+				_putchar(',');
+				_putchar (' ');
+			}
+			else
+			{
+				_putchar('0' + result / 10);
+				_putchar('0' + result % 10);
+				_putchar(',');
+				_putchar(' ');
+			}
+		 }
+		_putchar('\n');
+	}
+}
