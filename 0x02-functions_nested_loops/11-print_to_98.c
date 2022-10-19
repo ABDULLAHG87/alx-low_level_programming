92e402a78d74f728b1827d90c3059fc47755c335
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-19 15:54:34 -0700

    Task 11: 98 Battery Street

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * print_to_98 - print all natural numbers
@@ -8,19 +9,19 @@
 
 void print_to_98(int n)
 {
-	int i;
-
-	if(n < 98)
+	if (n < 98)
 	{
-		for (i = n; i <= 98; i++)
+		for ( ; n < 98; n++)
 		{
-			_putchar('0' + i / 10);
-			_putchar('0' + i % 10);
-			_putchar(',');
-			_putchar(' ');
+			printf("%d, ", n);
 		}
-		_putchar('\n');
-
 	}
-
+	else if (n > 98)
+	{
+		for ( ; n < 98; n--)
+		{
+			printf("%d, ", n);
+		}
+	}
+	printf("%d\n", n);
 }
