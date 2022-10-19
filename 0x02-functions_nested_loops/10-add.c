7772c189dea9fa14565812916fef5d5c9c37c530
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-19 04:16:13 -0700

    Task 10: a+b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - this function adds two integers
+ * @digit_1: integer 1;
+ * @digit_2: Integer 2;
+ * Return: 0 (Success)
+ */
+
+int add(int digit_1, int digit_2)
+{
+	int result;
+
+	result = digit_1 + digit_2;
+	return (result);
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,44 +15,30 @@ void times_table(void)
 	{
 		for (col = 0; col < 10; col++)
 		{
-			if (col == 0)
+	       		result = row * col;
+			if(col == 0)
 			{
-				result = row * col;
- 			       	_putchar('0' + result);
-		       		_putchar(',');
-		      		_putchar(' ');
+	       			_putchar('0' + result);
+       				_putchar(',');
+		       		_putchar(' ');
+
 			}
-			else if (col >= 0 && col != 9)
+       			else if(result <= 9)
 			{
-				result = row * col;
-				if (result <= 9)
-				{
-					_putchar(' ');
-					_putchar('0' + result);
-					_putchar(',');
-					_putchar(' ');
-				}
-				else
-				{
-					_putchar('0' + result / 10);
-					_putchar('0' + result % 10);
-					_putchar(',');
-					_putchar(' ');
-				}
-			  }
-			else
+		       		_putchar(' ');
+	       			_putchar('0' + result);
+       				_putchar(',');
+		       		_putchar(' ');
+			 }
+		       	else
+	       		{
+			       	_putchar('0' + result / 10);
+			       	_putchar('0' + result % 10);
+			       	_putchar(',');
+			       	_putchar(' ');
+		       	}
+			if  (col == 9)
 			{
-				result = row * col;
-				if (result <= 9)
-				{
-					_putchar(' ');
-					_putchar('0' + result);
-				}
-				else
-				{
-					_putchar('0' + result / 10);
-					_putchar('0' + result % 10);
-				}
 			}
 		}
 		_putchar('\n');
