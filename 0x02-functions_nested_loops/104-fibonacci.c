e80bd0413e08d57dad7ad8da9b4b3d0d15f9833a
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-19 18:48:23 -0700

    Task 16: first 100 fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,11 +8,11 @@
 int main(void)
 {
 	int i;
-	long int n1 = 1;
-	long int n2 = 2;
+	unsigned long int n1 = 1;
+	unsigned long int n2 = 2;
 	unsigned long int fib_num;
 
-	printf("%ld, %ld", n1, n2);
+	printf("%lu, %lu", n1, n2);
 	for (i = 0; i < 96; i++)
 	{
 		fib_num = n1 + n2;
