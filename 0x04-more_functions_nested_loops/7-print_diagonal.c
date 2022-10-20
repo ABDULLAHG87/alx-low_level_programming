7bab21b073be2cfbf3e6e02aa6638dccdec62b64
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-20 13:09:28 -0700

    Task 7: I feel like I am diagonally parked

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines
+ * @n: no of times
+ * Return: 0 (Success);
+ */
+
+void print_diagonal(int n)
+{
+	int row;
+	int col;
+
+	if (n <= 0)
+		_putchar('\n');
+
+	for (row = 1; row <= n; row++)
+	{
+		for (col = 1; col <= row; col++)
+		{
+			_putchar(' ');
+		}
+		_putchar('\\');
+		_putchar('\n');
+	}
+}
