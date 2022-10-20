9f304f871fe6bceb3095c59b6f539e66b3390535
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-10-20 13:56:36 -0700

    Task 8: You are so much sunshine

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_square - prints a square
+ * @size: size of a square
+ * Return: 0 (Success)
+ */
+
+void print_square(int size)
+{
+	int row;
+	int col;
+
+	if (size <= 0)
+		_putchar('\n');
+	for (row = 0; row < size; row++)
+	{
+		for (col = 0; col < size; col++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
