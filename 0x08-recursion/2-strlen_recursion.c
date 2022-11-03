cfc3f7a1cbc0c3d311aa95113d95533ce25704a9
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-03 07:31:03 -0700

    Task 2 - Strlen Recursive added

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Function the returns length of a string
+ * @s: Input String
+ * Return: length of String
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 +_strlen_recursion(s + 1));
+}
