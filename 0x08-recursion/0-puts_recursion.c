3a8b55e5d6dc121b2862b752a2a9d11396a96b35
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-03 03:44:06 -0700

    Task 0 - puts_Recursion Committed

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Function that prints a String
+ * @s: String
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
