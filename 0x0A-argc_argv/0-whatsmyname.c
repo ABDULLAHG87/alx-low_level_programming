c595a0781eab939646fd0be5aa4f0bb59f8e1d41
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-05 13:45:23 -0700

    Task 0 Committed

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,17 +2,18 @@
 
 /**
  * main - Entry Point
- * @argc - Argument Count
- * @argv - Argument Vector
- */
+ * @argc: Argument Count
+ * @argv: Argument Vector
+ * Return: 0 Always
+*/
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	for (i = 0; i < argc; i++)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
