ab6f68780673991d4b090e5726ef3c70f83b88fa
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-05 13:42:22 -0700

    Task 0 Committed

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - Entry Point
+ * @argc - Argument Count
+ * @argv - Argument Vector
+ */
+
+int main (int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n",argv[0]);
+	}
+	return (0);
+}
