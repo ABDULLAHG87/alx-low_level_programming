7a1642be1fb27eccf66c58c24307a6d562ec9439
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-09 06:55:25 -0800

    Task 2 Committed

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,9 +16,9 @@ char *str_concat(char *s1, char *s2)
 	int concat_index = 0;
 
 	if (s1 == NULL)
-		return (NULL);
+		s1 = "";
 	if (s2 == NULL)
-		return (NULL);
+		s2 = "";
 
 	for (index = 0; s1[index] || s2[index]; index++)
 	{
