011e0b47c2e932b28cd69d5a1d1bba90f7c0c86d
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-11 14:03:53 -0800

    Task 1 Committed

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,44 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - Concatenate two strings
- * @s1: string 1
- * @s2: String 2
- * @n: first n byte
- * Return: pointer (str)
+ * string_nconcat - Concatenate two strings using n amount of s2
+ * @s1: First string
+ * @s2: String to add to end of s1
+ * @n: Amount of s2 to add to s1
+ *
+ * Return: pointer to new area in memory, NULL if it fails
  */
-
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *concat;
-	unsigned int index, len1, len2;
-	unsigned int concat_index = 0;
+	char *nstr;
+	unsigned int i, len, j;
+	unsigned int size;
 
+	len = 0;
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	for (index = 0; s1[index] != '\0'; index++)
-		len1++;
-	for (index = 0; s2[index] != '\0'; index++)
-		len2++;
-	if (n >= len2)
-		n = len2;
-	concat = malloc(sizeof(*concat) * (len1 + n) + 1);
-
-	if (concat == NULL)
-	{
+	while (s1[len] != '\0')
+		len++;
+	size = (len + n) * sizeof(*nstr);
+	nstr = malloc(sizeof(*nstr) * ((len + n) + 1));
+	if (nstr == NULL)
 		return (NULL);
-	}
-	for (index = 0; s1[index]; index++)
+	i = 0;
+	while (i < size && s1[i] != '\0')
 	{
-		concat[concat_index++] = s1[index];
+		nstr[i] = s1[i];
+		i++;
 	}
-	for (index = 0; index < n; index++)
+	j = 0;
+	while (i < size && s2[j] != '\0')
 	{
-		concat[concat_index++] = s2[index];
+		nstr[i] = s2[j];
+		i++;
+		j++;
 	}
-	concat[concat_index] = '\0';
-	return (concat);
+	nstr[i] = '\0';
+	return (nstr);
 }
