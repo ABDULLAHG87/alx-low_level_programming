04be21c774b48430a340e61926dad9cec5f58738
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-15 12:10:34 -0800

    Task 0 - puppy committed

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Structures for dog
+ * @name: Name of Dog
+ * @age: Age of Dog
+ * @owner: Owner of Dog
+ *
+ * Description: This structure encapsulate all chacteristics of a dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
