876090daea8ddbab7c2cb923e30c734ebc596d92
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-11-29 14:26:55 -0800

    BUILD: Task 8 - Sum List

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Function that returns sum of all data
+ * @head: Head of the linked list
+ * Return: sum;
+ */
+
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	if (head == NULL)
+	{
+		return (-1);
+	}
+	if (head != NULL)
+	{
+		while (head != NULL)
+		{
+			sum += head->n;
+			head = head->next;
+		}
+		return (sum);
+	}
+	else
+		return (0);
+}
