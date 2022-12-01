12be67680df931c07270398292ecd567c1f7d4d4
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-12-01 13:04:33 -0800

    BUGFIX: Fix issue of index greater than required size
    A fix was applied to enable the code flag and error if the size of the
    index specified is above 8 bits

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	int get_status;
 
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
 	get_status = (n >> index) & 1;
 
 	return (get_status);
