780fb742fb9806720644f9f82eb03af1185f5ed9
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-12-01 13:39:54 -0800

    BUILD: create function that clears bit at a given index
    A function that clears bit at a given index was created.

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * clear_bit - Function that clears the value of a bit to 1 at a given index
+ * @n: Integer
+ * @index: Index to be set
+ * Return: set bit (1), (-1) if error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (n == NULL)
+	{
+		return (-1);
+	}
+
+	if (index >= (sizeof(unsigned long int) * 8))
+	{
+		return (-1);
+	}
+
+	*n = (~(1 << index)) & *n;
+
+	return (1);
+}
