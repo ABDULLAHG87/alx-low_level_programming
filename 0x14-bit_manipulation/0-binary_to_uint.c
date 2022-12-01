7b422b730b28186767e5b1169b6a718e79eaa63d
ABDULLAHG87 <hakeemabdullah87@gmail.com>
2022-12-01 09:18:50 -0800

    BUILD: Create Task 0-0
    This task involves coding to convert binary numbers to Decimal Numbers using Bit
    manipulation.

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * _strlen - Function that converts Binary to Decimal
+ * @b: Binary String
+ * Return: len
+ */
+
+unsigned int _strlen(const char *b)
+{
+	int len = 0;
+	int index = 0;
+
+	while (b[index])
+	{
+		len++;
+		index++;
+	}
+	return (len);
+}
+
+/**
+ * binary_to_uint - Function that converts Binary to Decimal
+ * @b: Binary String
+ * Return: decimal
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int sum = 0;
+	unsigned int dec_val = 1;
+	unsigned int blen = 0;
+	int index;
+
+	if (b == NULL)
+	{
+		return (-1);
+	}
+
+	blen = _strlen(b);
+	for (index = (blen - 1); index >= 0; index--)
+	{
+		if (b[index] == '1')
+		{
+			sum += dec_val;
+		}
+		else if (b[index] == '0')
+		{
+			sum = sum;
+		}
+		else
+			return (0);
+		dec_val *= 2;
+	}
+	return (sum);
+}
---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include<stdlib.h>
+unsigned int _strlen(const char *b);
+unsigned int binary_to_uint(const char *b);
+#endif
